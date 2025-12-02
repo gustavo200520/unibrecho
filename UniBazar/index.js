a829edfce82d4c7085189d532ebd532dd925f8ee
gustavo200520 <gustavomatosbarbosa2@gmail.com>
2025-12-01 22:59:22 -0300

    botao

--- UniBazar/index.js
+++ UniBazar/index.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btngerenciar_anuncio').addEventListener('click', () => {
         window.location.href = 'tela_gerenciamento_de_anuncio.html';
     });
+
+    document.getElementById('btnlista_de_desejos').addEventListener('click', () => {
+        window.location.href = 'tela_lista_de_desejos.html';
+    });
 });
