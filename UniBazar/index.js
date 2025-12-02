0ba75d433c28e652d20e5526c9e6f8a360f2ca45
gustavo200520 <gustavomatosbarbosa2@gmail.com>
2025-12-01 22:42:18 -0300

    sarve

---
+++ UniBazar/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btngerenciar_anuncio').addEventListener('click', () => {
+        window.location.href = 'tela_gerenciamento_de_anuncio.html';
+    });
+});
